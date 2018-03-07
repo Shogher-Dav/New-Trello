0631573bf6bf2776795af66015d172ef85be2085
shogher <shogher.davtyan95@gmail.com>
2018-03-07 07:53:20 +0400

    close modalBoard

--- js/board.js
+++ js/board.js
@@ -34,15 +34,22 @@ function createBoard() {
 
 function myFunction() {
     var param = event.currentTarget.text;
-    window.location = 'file:///C:/Users/User/Desktop/trello/trello/main.html?Board-Name=' + param;
+    window.location = 'file:///C:/Users/User/Desktop/trello/main.html?Board-Name=' + param;
+    // window.location = 'main.js' + param;
+
 
 }
 
 function myBoards() {
-    let div = document.createElement("DIV");
-    div.classList.add("modalDiv");
-    div.id = "modalDiv";
-    document.body.appendChild(div);
+   let div = document.createElement("DIV");
+   let button = document.createElement("span");
+   let btnText=document.createTextNode("X");
+   button.appendChild(btnText);
+   button.classList.add('closeButton');
+   div.appendChild(button);
+   div.classList.add("modalDiv");
+   div.id = "modalDiv";
+   document.body.appendChild(div);
     for (var i = 0; i < localStorage.length - 1; i++) {
         let boardName = document.createElement("a");
         boardName.onclick = myFunction;
@@ -52,4 +59,39 @@ function myBoards() {
         document.getElementById('modalDiv').appendChild(boardName);
 
     }
+    button.addEventListener("click", showAndHide);
+ 
+  //window.addEventListener("click", windowOnClick);
+}
+ function showAndHide(){
+//     let trigger = document.querySelector(".triggerBoard");
+//     let modalBoard = document.querySelector(".modalDiv");
+
+//     function toggleModal() {
+//         modalBoard.classList.toggle(".show-modalBoard");
+//     }
+
+//     function windowOnClick(event) {
+//         if (event.target === modalBoard) {
+//             toggleModal();
+//         }
+//     }
+
+//     trigger.addEventListener("click", toggleModal);
+//     window.addEventListener("click", windowOnClick);
+//     modalDiv.addEventListener("click", toggleModal);
+    
+    //createBoard.addEventListener("click", toggleModal);
+
+
+    let trigger = document.querySelector(".triggerBoard");
+    let closeButton=document.querySelector(".closeButton");
+    let modalBoard=document.querySelector(".modalDiv");
+    function toggleModal()
+    {
+        modalBoard.classList.toggle("show-modalBoard");
+    }
+   
+    trigger.addEventListener("click",toggleModal);
+    closeButton.addEventListener("click",toggleModal);
 }
