5721ea8bdecec0e10fdd438ef9e7a2afc18782ba
shogher <shogher.davtyan95@gmail.com>
2018-03-08 05:59:42 +0400

    Modal working for boards name and created function

--- js/board.js
+++ js/board.js
@@ -3,9 +3,15 @@ window.onload = function() {
     let modal = document.querySelector(".modal");
     let trigger = document.querySelector(".trigger");
     let createBoard = document.querySelector(".createBoard");
+    let modalBoard=document.querySelector(".modalBoard");// modal for Boards name
+    let triggerBoard = document.querySelector(".triggerBoard");// My Boards button
+    let closeButton=document.querySelector(".closeButton");// closeButton
 
     function toggleModal() {
         modal.classList.toggle("show-modal");
+    }
+    function toggleBoardModal() {
+        modalBoard.classList.toggle("show-modal");
     }
 
     function windowOnClick(event) {
@@ -17,6 +23,9 @@ window.onload = function() {
     trigger.addEventListener("click", toggleModal);
     window.addEventListener("click", windowOnClick);
     createBoard.addEventListener("click", toggleModal);
+    triggerBoard.addEventListener("click",toggleBoardModal);
+    closeButton.addEventListener("click",toggleBoardModal);
+    myBoards();
 }
 
 
@@ -41,57 +50,14 @@ function myFunction() {
 }
 
 function myBoards() {
-   let div = document.createElement("DIV");
-   let button = document.createElement("span");
-   let btnText=document.createTextNode("X");
-   button.appendChild(btnText);
-   button.classList.add('closeButton');
-   div.appendChild(button);
-   div.classList.add("modalDiv");
-   div.id = "modalDiv";
-   document.body.appendChild(div);
     for (var i = 0; i < localStorage.length - 1; i++) {
         let boardName = document.createElement("a");
         boardName.onclick = myFunction;
         boardName.classList.add("modalBoardName");
         var text = document.createTextNode(localStorage.key(i));
         boardName.appendChild(text);
-        document.getElementById('modalDiv').appendChild(boardName);
+        document.querySelector(".modalBoard").appendChild(boardName);
 
     }
-    button.addEventListener("click", showAndHide);
+}
  
-  //window.addEventListener("click", windowOnClick);
-}
- function showAndHide(){
-//     let trigger = document.querySelector(".triggerBoard");
-//     let modalBoard = document.querySelector(".modalDiv");
-
-//     function toggleModal() {
-//         modalBoard.classList.toggle(".show-modalBoard");
-//     }
-
-//     function windowOnClick(event) {
-//         if (event.target === modalBoard) {
-//             toggleModal();
-//         }
-//     }
-
-//     trigger.addEventListener("click", toggleModal);
-//     window.addEventListener("click", windowOnClick);
-//     modalDiv.addEventListener("click", toggleModal);
-    
-    //createBoard.addEventListener("click", toggleModal);
-
-
-    let trigger = document.querySelector(".triggerBoard");
-    let closeButton=document.querySelector(".closeButton");
-    let modalBoard=document.querySelector(".modalDiv");
-    function toggleModal()
-    {
-        modalBoard.classList.toggle("show-modalBoard");
-    }
-   
-    trigger.addEventListener("click",toggleModal);
-    closeButton.addEventListener("click",toggleModal);
-}
