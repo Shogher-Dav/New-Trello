50816eb74ffebf1d02c4c61903155721d695db0f
shogher <shogher.davtyan95@gmail.com>
2018-03-11 18:15:14 +0400

    add delete function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-class Creat {
+class Create {
     constructor(elName, identification, count, parent) {
         this.el = document.createElement(elName);
         this.el.classList.add(identification);
@@ -21,7 +21,7 @@ class Creat {
     }
     tickMethod(){
 
-       function x(){
+   function x(){
          this.el.classList.toggle("tick1");
  }
   this.el.addEventListener('click',x.bind(this));
@@ -47,11 +47,23 @@ function read() {
     for (let i = 1; i <= length; i++) {
         openCreateBox();
         for (let j = 0; j < obj['addList' + i].length; j++) {
-            let task = new Creat("SPAN", 'list-text', '', "addList" + i);
-            let tick=new Creat('img','tick','',  "addList" + i);
+            let task = new Create("SPAN", 'list-text', '', "addList" + i);
+            let tick=new Create('img','tick','',  "addList" + i);
+            let garbage=new Create('image','garbage','',"addList" + i );
             task.span(obj['addList' + i][j]);     
-            tick.tickMethod();  
-           
+          //  tick.tickMethod();  
+          function x()
+          {
+         //  tick.el.classList.value='tick1';
+           tick.el.classList.toggle('tick1');
+            let z=tick.el.classList;
+          return z;
+          }
+           let y= tick.el.addEventListener('click',x)
+          //  console.log(tick.el.classList);
+          //  alert(tick.el.classList.value);
+         // console.log(tick.el.classList);
+          //console.log(y);
         }
     }
 }
@@ -62,33 +74,66 @@ function openCreateBox() {
     var id = "board";
     count++;
     //addList
-    var list = new Creat("DIV", "addList", count, id);
+    var list = new Create("DIV", "addList", count, id);
     id = list.id;
-       
+     //  console.log(id);
 
-    var input = new Creat("INPUT", 'addInput', count, id);
+    var input = new Create("INPUT", 'addInput', count, id);
     input.input('text');
 
-    var saveBtn = new Creat("BUTTON", 'btnList', count, id);
+    var saveBtn = new Create("BUTTON", 'btnList', count, id);
     saveBtn.button("onclick", "save(id)", 'save');
 
-    var delBtn = new Creat("BUTTON", 'btnDelete', count, id);
-    delBtn.button("onclick", "delete(id)", 'x');
+    var delBtn = new Create("BUTTON", 'btnDelete', count, id);
+    delBtn.button("onclick", "deleteList(id)", 'x');
 
 }
+function deleteList(id){
+    let deleteList = document.getElementById(id); // vercnume delBtn-i id
+    let list=deleteList.parentNode; // gtnum enq list-y
 
+    deleteList.parentNode.parentNode.removeChild(list); // DOM-ic jnjum en list-y
+
+    //from local storage
+    var obj = JSON.parse(localStorage.getItem(boardName)); //lokal storage-ic vercnum enq objecty
+    let idNum = list.id.replace(/[^0-9]/g, ''); // stanum em addList1... list id-i tivy;
+    console.log(obj);
+     delete obj[list.id]; // jnjum enq  listy objecti mijic
+     
+////  Poxum enq addList-i tivy mihatov pakasacnum enq/////////// 
+    renameProp =  (
+        oldProp,
+        newProp,
+    { [oldProp]: old,
+     ...others 
+      }) => ({
+        [newProp]: old,
+        ...others
+    })          
+
+    for(let i=idNum; i<obj.length; i++){
+        idNum++;
+        obj=renameProp('addList'+idNum, 'addList'+i, obj); 
+       
+      }
+     
+    obj.length-=1;
+    localStorage.setItem(boardName, JSON.stringify(obj));
+}
 
 function save(id) {
     let idNum = id.replace(/[^0-9]/g, '');
     let inputTextId = "addInput" + idNum;
     let inputText = document.getElementById(inputTextId).value;
     let addListId = 'addList' + idNum;
-    let task = new Creat("SPAN", 'list-text', '', addListId);
-    let tick=new Creat("img",'tick',"",addListId);
-    task.span(inputText,tick);
-  
+    let task = new Create("span", 'list-text', '', addListId);
+    let tick=new Create("img",'tick','',addListId);
+  //  let garbage=new Create('image','garbage','', addListId)  avelacnum e garbage-i nkar
+    task.span(inputText);
+  //  garbage.button('onclick','deleteTask(id)'); // buton a vra dnum a onclick ev deletTask atributy 
 
-   tick.tickMethod();
+   tick.tickMethod(); //poxum e tick-i classy
+
  
     //set in localStorage
     let obj = JSON.parse(localStorage.getItem(boardName));
@@ -110,6 +155,29 @@ function save(id) {
     //   dragAndDrop(text,list);
   
 }
+
+
+// function deleteTask(id){
+
+//    garbageImage.addEventListener("click", function deleteTask(){
+//        let listElement = document.getElementById(garbageImage.parentElement.id);
+//        console.log(id.parentNode)
+//        listElement.parentNode.removeChild(listElement);
+  
+//       //  from local storage
+//         var obj = JSON.parse(localStorage.getItem("board1"));
+//         let listId = document.getElementById(addListId).id;
+//         let item = garbageImage.parentNode.innerText;
+//         let index = obj[listId].indexOf(item);
+//         obj[listId].splice(index, 1);
+//         localStorage.setItem("board1", JSON.stringify(obj));
+//         obj = JSON.parse(localStorage.getItem('board1'));
+//    });
+
+// }
+
+
+
 
 // function dragAndDrop(classTask,classList){
 
